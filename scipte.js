47f7e2da5474600ac2e7e1565d5e6db89004f7bb
2003GG <mohamadzat6@gmail.com>
2025-11-02 16:04:51 +0100

    i have a problem in the events

--- scipte.js
+++ scipte.js
@@ -5,133 +5,120 @@ let answer4 = document.getElementById("answer__4");
 
 let qes = document.getElementById("question");
 let my_button = document.getElementById("suivant");
-// let dark = document.getElementByClassName("dark__mood");
-
-// for the dark mood
-// dark.addEventListener("click",(event)=>{
-//   event.target.Body.style.backgroundColor = "rgb(43, 43, 43)";
-// });
 
 const obj = [
   {
     Qestion: "who won the world cup 2006",
-    answer: ["argentina", "france", "italy", "spain"]
+    answer: ["argentina", "france", "italy", "spain"],
+    
    
   },
   {
     Qestion: "who won the chapions league in 2004",
-    answer: ["Porto", "mancherster united", "FC barcalona", "liverpool"]
+    answer: ["Porto", "mancherster united", "FC barcalona", "liverpool"],
+     
    },
 
   {
     Qestion: "the ballon d'or winner in 2003 is",
-    answer: ["Messi", "Ronaldinho", "buffon", "Nedvěd"]
+    answer: ["Messi", "Ronaldinho", "buffon", "Nedvěd"],
+      
   },
-
   {
-   Tr: ["italy", "porto", "Nedvěd"]
-   }
+    Tr: ["Nedvěd","porto","italy"]
+  }
+
+ 
 ];
 
 
-    my_button.addEventListener("click",changeqe)
+    my_button.addEventListener("click",changeqe);
     function changeqe(){
         
-          for (let i=0; i <obj.length; i++){
-                 qes.textContent = obj[0].Qestion
+          let i=0;
+           
+                 qes.textContent = obj[i].Qestion
             
-                answer1.textContent = obj[0].answer[0]
-                answer2.textContent = obj[0].answer[1]
-                answer3.textContent = obj[0].answer[2]
-                answer4.textContent = obj[0].answer[3]
-            
+                answer1.textContent = obj[i].answer[0]
+                answer2.textContent = obj[i].answer[1]
+                answer3.textContent = obj[i].answer[2]
+                answer4.textContent = obj[i].answer[3]
                
-              }
+                
+              
             }
             
             
             
-            
-            // resersh for the correct answer
-            
-            //   for (let i=0; i <obj.length; i++){
-            //      qes.innerText = obj[0].Qestion
-            //      for(let j=0;j<obj.length;j++){
-            //     answer1.textContent = obj[i].answer[j]
-            //     answer2.textContent = obj[i].answer[j]
-            //     answer3.textContent = obj[i].answer[j]
-            //     answer4.textContent = obj[i].answer[j]
-            // }
-            //     console.log(obj[i])
-            //   }
-            // }
-            
-            for (let i = 0; i < obj.length; i++){
-                if (answer1 === obj[i].Tr){
-    answer1.addEventListener('click', event => {
+            for (let j = 0; j < obj.length; j++){
+              
+ answer1.addEventListener('click', event => {
+                if (answer1 === Tr){
+      
       answer1.style.backgroundColor = " rgb(75, 255, 96)";
-      changeqe();
-    });
-  }
-
-  if (answer1 !== obj[i].Tr) {
-    answer1.addEventListener('click', event => {
+      
+    }
+    else {
+    
       answer1.style.backgroundColor = "rgb(255, 92, 92)";
-      changeqe();
-    });
-  }
+     
+    }
+  changeqe();
+  i++;
 
-  if (answer2 === obj[i].Tr) {
-    answer2.addEventListener('click', event=> {
+        });
+
+ 
+  
+
+
+answer2.addEventListener('click', event => {
+  if (answer2 === Tr) {
+    
       answer2.style.backgroundColor = " rgb(75, 255, 96)";
-      changeqe();
-    });
-  }
+     
+      }
 
-  if (answer2 !== obj[i].Tr) {
-    answer2.addEventListener('click', event => {
-      answer3.style.backgroundColor = "rgb(255, 92, 92)";
+ else {
+    
+      answer2.style.backgroundColor = "rgb(255, 92, 92)";
+    
+    }
       changeqe();
-    });
-  }
+      i++;
+  });
 
-  if (answer3 === obj[i].Tr) {
-    answer3.addEventListener('click', event => {
+
+  answer3.addEventListener('click', event => {
+  if (answer3 === Tr) {
+   
       answer3.style.backgroundColor = " rgb(75, 255, 96)";
-      changeqe();
-    });
-  }
-
-  if (answer3 !== obj[i].Tr) {
-    answer3.addEventListener('click' ,event => {
+     
+    }
+  
+  else {
+   
       answer3.style.backgroundColor = "rgb(255, 92, 92)";
-       changeqe();
-    });
-  }
+      
+    }
+     changeqe();
+     i++;
+  });
 
-  if (answer4 === obj[i].Tr) {
-    answer4.addEventListener('click', event => {
+
+answer4.addEventListener('click', event => {
+  if (answer4 === Tr) {
+    
       answer4.style.backgroundColor = " rgb(75, 255, 96)";
-       changeqe();
-    });
-  }
-
-  if (answer4 !== obj[i].Tr) {
-    answer4.addEventListener('click', event => {
+     
+    }
+  
+ else{
+    
       answer4.style.backgroundColor = "rgb(255, 92, 92)";
+   
+    }
        changeqe();
-    });
-  }
+       i++;
+  });
 }
-
-// function changecolor(event){
-//     event.target.style.backgroundcolor="red";
-//     event.target.textcontent="false";
-// }
-// answer1.addEventLister("click",changecolor);
-
-// if (
-//   answer1.addEventListener("mouseover", (event) => {
-//     event.target.style.backgroundColor = "black";
-//   })
-// );
